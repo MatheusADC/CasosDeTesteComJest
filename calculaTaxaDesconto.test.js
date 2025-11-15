5c9936ba90a2fc82099fccad6cc02c00f9e92591
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-11-15 19:05:23 -0300

    feat: Casos de teste para os mutantes que sobreviveram

--- calculaTaxaDesconto.test.js
+++ calculaTaxaDesconto.test.js
@@ -38,3 +38,33 @@ test("valorCompra >= 200 || tipoCliente == \"bronze\" - Todos os Caminhos Básic
 test("Nenhuma condição atendida - retorna 0", () => {
     expect(calculaTaxaDesconto(false, "", 100)).toBe(0);
 });
+
+// Matar os mutantes que sobreviveram
+
+test("valorCompra == 500", () => {
+    expect(calculaTaxaDesconto(false, "bronze", 500)).toBe(15);
+});
+
+test("tipoCliente == \"ouro\"", () => {
+    expect(calculaTaxaDesconto(false, "ouro", 190)).toBe(15);
+});
+
+test("primeiraCompra === true", () => {
+    expect(calculaTaxaDesconto(true, "", 190)).toBe(10);
+});
+
+test("valorCompra == 400", () => {
+    expect(calculaTaxaDesconto(false, "", 400)).toBe(10);
+});
+
+test("valorCompra >= 400 || tipoCliente == \"\" || primeiraCompra === false", () => {
+    expect(calculaTaxaDesconto(false, "", 490)).toBe(10);
+});
+
+test("valorCompra >= 200", () => {
+    expect(calculaTaxaDesconto(false, "", 290)).toBe(5);
+});
+
+test("valorCompra == 200", () => {
+    expect(calculaTaxaDesconto(false, "", 200)).toBe(5);
+});
